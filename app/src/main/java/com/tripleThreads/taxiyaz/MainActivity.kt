01941adc4d7a754fbed2f95a28a20bc9c12cdd95
paix <ghiwotselam@gmail.com>
2019-06-07 15:43:52 +0300

    Location Permission

--- app/src/main/java/com/tripleThreads/taxiyaz/MainActivity.kt
+++ app/src/main/java/com/tripleThreads/taxiyaz/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.tripleThreads.taxiyaz
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.opengl.Visibility
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.navigation.navOptions
@@ -15,27 +20,33 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var userViewModel: UserViewModel
 
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         userViewModel = ViewModelProviders.of(this).get(UserViewModel::class.java)
 
-        setContentView(R.layout.activity_main)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        bottom_navigation.visibility = View.INVISIBLE
+            setContentView(R.layout.activity_main)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        findNavController(R.id.nav_host).navigate(R.id.loading_fragment_dest)
 
-        if (userViewModel.user == null) {
-            findNavController(R.id.nav_host).navigate(R.id.login_fragment_dest)
-        }
-        else {
-            findNavController(R.id.nav_host).navigate(R.id.route_fragment_dest)
-            bottom_navigation.visibility = View.VISIBLE
-        }
+            bottom_navigation.visibility = View.INVISIBLE
 
-        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+            findNavController(R.id.nav_host).navigate(R.id.loading_fragment_dest)
+
+            if (userViewModel.user == null) {
+                findNavController(R.id.nav_host).navigate(R.id.login_fragment_dest)}
+
+            else {
+                findNavController(R.id.nav_host).navigate(R.id.route_fragment_dest)
+                bottom_navigation.visibility = View.VISIBLE
+            }
+
+            bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
     }
 
     val options = navOptions {
--- app/src/main/java/com/tripleThreads/taxiyaz/fragments/LoginFragment.kt
+++ app/src/main/java/com/tripleThreads/taxiyaz/fragments/LoginFragment.kt
@@ -1,7 +1,9 @@
 package com.tripleThreads.taxiyaz.fragments
 
+import android.Manifest
 import android.app.AlertDialog
 import android.content.DialogInterface
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,9 +11,10 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.fragment.findNavController
 import com.tripleThreads.taxiyaz.R
 import com.tripleThreads.taxiyaz.data.user.User
@@ -27,6 +30,57 @@ class LoginFragment : Fragment() {
     lateinit var phoneNumber: EditText
     lateinit var userName: EditText
     lateinit var registerButton: Button
+    private val locationAccessKey = 1
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationAccessKey) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                findNavController().navigate(R.id.route_fragment_dest)
+                activity!!.bottom_navigation.visibility = View.VISIBLE
+
+            } else {
+
+            }
+        }
+    }
+
+    private fun checkPermission(){
+
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this.requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+
+            val builder = AlertDialog.Builder(this.requireContext())
+            builder.setTitle("Required Permissions")
+            builder.setMessage("This app requires location permission to  guide you through different routes. " +
+                    "Please grant the permission.")
+            builder.setPositiveButton("Ok")
+            { dialog, which ->
+               requestPermissions(
+                   arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    locationAccessKey
+                )
+            }
+            builder.setNegativeButton(
+                "Cancel"
+            ) { dialog, which -> dialog.cancel() }
+            val alert = builder.create()
+            alert.show()
+        } else {
+
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                locationAccessKey
+            )
+
+
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,8 +95,17 @@ class LoginFragment : Fragment() {
         registerButton = view.ContinueBtn
 
 
+        if (ContextCompat.checkSelfPermission(this.requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            checkPermission()
+
+        }
+
+
         registerButton.setOnClickListener{
             showDialog(getFields())
+
         }
 
         return view
@@ -65,10 +128,32 @@ class LoginFragment : Fragment() {
             dialog.setMessage("Are you sure (+251) ${user.phoneNumber} is your number?   ")
             dialog.setPositiveButton("Yes", DialogInterface.OnClickListener{ dialog, which ->
                 // replace fragment
+                if (ContextCompat.checkSelfPermission(this.requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED
+                ) {
+                    userViewModel.insert(user)
+                    findNavController().navigate(R.id.route_fragment_dest)
+                    activity!!.bottom_navigation.visibility = View.VISIBLE
+                }
+                else{
+                    val builder = AlertDialog.Builder(this.requireContext())
+                    builder.setTitle("Required Permissions")
+                    builder.setMessage("In order to go further you need to allow the location. PLease do as you are asked")
+                    builder.setPositiveButton("Ok")
+                    { _, _ ->
+                        requestPermissions(
+                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                            locationAccessKey
+                        )
+                        userViewModel.insert(user)
+                    }
+                    builder.setNegativeButton(
+                        "Cancel"
+                    ) { _, _ -> dialog.cancel() }
+                    val alert = builder.create()
+                    alert.show()
 
-                userViewModel.insert(user)
-                findNavController().navigate(R.id.route_fragment_dest)
-                activity!!.bottom_navigation.visibility = View.VISIBLE
+                }
 
             })
             dialog.setNegativeButton("No", DialogInterface.OnClickListener{ dialog, which ->
